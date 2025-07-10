7b78262c9c7e85d37ffcd0bbc50670a68082232c
keshurgojiya <gojiyakeshur555@gmail.com>
2025-07-10 23:29:14 +0530

    contact page changes

--- src/components/ContactDetails.tsx
+++ src/components/ContactDetails.tsx
@@ -1,27 +1,42 @@
+import { MapPin, Phone, Mail } from "lucide-react";
+
 const ContactDetails = () => {
   return (
-    <div>
-      <p className="text-yellow-500 uppercase text-sm mb-2">Get In Touch</p>
-      <h2 className="text-3xl font-semibold mb-4">Contact Us</h2>
-      <div className="space-y-4">
-        <div>
-          <p className="font-bold">Site Address:</p>
-          <p>
-            Embrace The Wind, B/h McDonald&apos;s On S.G. Highway, Sargasan
-            Cross Road, Sargasan, Gandhinagar - 382421.
-          </p>
+  <div className="bg-white-100 p-6 rounded-lg shadow-md">
+      <h2 className="text-3xl font-semibold mb-6 text-gray-800 border-l-4 border-orange-500 pl-4">
+        Contact Us
+      </h2>
+
+      <div className="space-y-6 text-gray-700">
+        <div className="flex items-start gap-4">
+          <MapPin className="text-orange-500 mt-1" />
+          <div>
+            <p className="font-semibold">Address:</p>
+            <p>
+              Embrace The Wind, B/h McDonald's On S.G. Highway, Sargasan Cross
+              Road, Sargasan, Gandhinagar - 382421.
+            </p>
+          </div>
         </div>
-        <div>
-          <p className="font-bold">Phone:</p>
-          <p>+91 9797976577</p>
+
+        <div className="flex items-start gap-4">
+          <Phone className="text-orange-500 mt-1" />
+          <div>
+            <p className="font-semibold">Phone:</p>
+            <p>+91 9797976577</p>
+          </div>
         </div>
-        <div>
-          <p className="font-bold">Email:</p>
-          <p>Embracethewind33@gmail.com</p>
+
+        <div className="flex items-start gap-4">
+          <Mail className="text-orange-500 mt-1" />
+          <div>
+            <p className="font-semibold">Email:</p>
+            <p>Embracethewind33@gmail.com</p>
+          </div>
         </div>
       </div>
     </div>
-  );
+    );
 };
 
 export default ContactDetails;
--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -18,6 +18,16 @@ const ContactSection = () => {
               className="rounded-lg object-cover"
             />
           </div>
+          {/* <div className="w-full h-96">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3667.3439257996515!2d72.61613147509716!3d23.19413357905394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395c2b88c9293683%3A0x76f11a7d3831f354!2sEmbrace%20The%20Wind!5e0!3m2!1sen!2sin!4v1750259842747!5m2!1sen!2sin"
+              width="100%"
+              height="100%"
+              className="rounded-md border-2 border-gray-200"
+              allowFullScreen
+              loading="lazy"
+            ></iframe>
+          </div> */}
           <ContactDetails />
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
