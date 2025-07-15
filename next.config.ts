405512d29b39e7b5b4a6a764f01df9591687ba2d
sbharvadiya <sagarbharvadiya3113@gmail.com>
2025-07-15 10:31:00 +0530

    remove items

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,16 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    formats: ['image/avif', 'image/webp'], // Enables modern formats
-    domains: ['images.pexels.com'],        // External image host allowed
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+    ],
   },
   reactStrictMode: true,
-  swcMinify: true,
 };
+
 
 export default nextConfig;
